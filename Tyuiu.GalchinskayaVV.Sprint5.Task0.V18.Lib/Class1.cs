3d15a35eedaffc077a82f52a3ffba627f759e8a6
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-11-22 11:34:40 +0500

    Add project files.

---
+++ Tyuiu.GalchinskayaVV.Sprint5.Task0.V18.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint5.Task0.V18.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint5.Task0.V18.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint5.Task0.V18.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
